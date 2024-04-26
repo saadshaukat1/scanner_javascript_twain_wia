c34df3a4c63dc032d431a08e221bf0a1a93bcd0b
Saad <msaadshaukat@gmail.com>
2024-04-26 17:08:25 +0500

    Fix comment in scan.bat file

--- commands/scan.bat
+++ commands/scan.bat
@@ -24,6 +24,6 @@ REM --progress: Show progress bar / --force: Replace existing file
 REM Run scanner and then -o :Output file (output.jpg) in "/Output" folder 
 START /W /B ../NAPS2.Console.exe -o ../Output/output.pdf --progress 
 REM Covert the actual file to pdf
-START /W /B ../NAPS2.Console.exe -i ../Output/output.pdf -n 0 -o ../Output/output.jpg 
+REM START /W /B ../NAPS2.Console.exe -i ../Output/output.pdf -n 0 -o ../Output/output.jpg 
 
 EXIT
